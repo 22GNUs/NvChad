3371370cdfbbbd17793298e59acd5041f3e077fa
wxh <stormaroon@gmail.com>
2022-10-20 19:12:40 +0800

    Update null-ls config && ts support

--- lua/custom/plugins/lspconfig.lua
+++ lua/custom/plugins/lspconfig.lua
@@ -2,7 +2,7 @@ local on_attach = require("plugins.configs.lspconfig").on_attach
 local capabilities = require("plugins.configs.lspconfig").capabilities
 
 local lspconfig = require "lspconfig"
-local servers = { "jsonls", "gopls" }
+local servers = { "jsonls", "gopls", "tsserver" }
 
 for _, lsp in ipairs(servers) do
   lspconfig[lsp].setup {

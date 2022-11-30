5c27d5eb3a37b0f7e4ad3bed652fc90478456260
22GNUs <stormaroon@gmail.com>
2022-11-30 23:48:53 +0800

    Fix auto format on save

--- lua/custom/plugins/null-ls.lua
+++ lua/custom/plugins/null-ls.lua
@@ -4,6 +4,7 @@ if not present then
   return
 end
 local b = null_ls.builtins
+local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
 
 local sources = {
 
@@ -20,7 +21,7 @@ local sources = {
 
   -- Es
   b.formatting.eslint,
-  b.diagnostics.eslint
+  b.diagnostics.eslint,
 }
 
 local M = {}
@@ -31,9 +32,17 @@ M.setup = function()
     sources = sources,
 
     -- format on save
-    on_attach = function(client)
-      if client.server_capabilities.document_formatting then
-        vim.cmd "autocmd BufWritePre <buffer> lua vim.lsp.buf.formatting_sync()"
+    -- you can reuse a shared lspconfig on_attach callback here
+    on_attach = function(client, bufnr)
+      if client.supports_method "textDocument/formatting" then
+        vim.api.nvim_clear_autocmds { group = augroup, buffer = bufnr }
+        vim.api.nvim_create_autocmd("BufWritePre", {
+          group = augroup,
+          buffer = bufnr,
+          callback = function()
+            vim.lsp.buf.format { bufnr = bufnr }
+          end,
+        })
       end
     end,
   }

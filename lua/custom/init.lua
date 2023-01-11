fe2dc3eb69e36d73a660316e541bb383c8feb019
22GNUs <stormaroon@gmail.com>
2023-01-11 16:07:10 +0800

    Add BufCurOnly command binding

--- lua/custom/init.lua
+++ lua/custom/init.lua
@@ -1,4 +1,7 @@
 vim.wo.wrap = false
+-- close other buffers and keep current opened only
+-- see: https://tech.serhatteker.com/post/2020-06/close-all-buffers-but-current-in-vim/
+vim.cmd "silent! command! BufCurOnly execute '%bdelete|edit#|bdelete#'"
 
 -- neovide
 require "custom.gui.neovide"

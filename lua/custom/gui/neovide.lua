51b23d01e7e10193f3e23ffd73a167511b340171
22GNUs <stormaroon@gmail.com>
2023-01-12 11:22:38 +0800

    Set neovide transparency

--- lua/custom/gui/neovide.lua
+++ lua/custom/gui/neovide.lua
@@ -1,13 +1,15 @@
 local g = vim.g
 if g.neovide ~= nil then
   vim.opt.guifont = { "Iosevka Mayukai Original", ":h18" }
-  -- g.neovide_transparency = 0.8
-  -- g.neovide_cursor_trail_length = 0.00
-  -- g.neovide_cursor_animation_length = 0
+  g.neovide_transparency = 0.0
+  g.transparency = 0.98
+  vim.cmd "let g:neovide_background_color = '#16161f'.printf('%x', float2nr(255 * g:transparency))"
   g.neovide_refresh_rate = 60
   g.neovide_hide_mouse_when_typing = true
   g.neovide_remember_dimensions = true
   g.neovide_remember_window_size = true
+  g.neovide_floating_blur_amount_x = 2.0
+  g.neovide_floating_blur_amount_y = 2.0
   -- vfx mode
   -- g.neovide_cursor_vfx_mode = "ripple"
   g.neovide_cursor_vfx_mode = "railgun"

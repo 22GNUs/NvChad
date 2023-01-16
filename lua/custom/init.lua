f68e3c155f3e073bb323c890f84db6590527d78a
22GNUs <stormaroon@gmail.com>
2023-01-16 16:49:00 +0800

    Refactor blankline configurations

--- lua/custom/init.lua
+++ lua/custom/init.lua
@@ -1,17 +1,5 @@
 vim.wo.wrap = false
-
--- for blankline
-vim.cmd [[highlight IndentBlanklineIndent1 guifg=#E06C75 gui=nocombine]]
-vim.cmd [[highlight IndentBlanklineIndent2 guifg=#C097FC gui=nocombine]]
-vim.cmd [[highlight IndentBlanklineIndent3 guifg=#56B6C2 gui=nocombine]]
-vim.cmd [[highlight IndentBlanklineIndent4 guifg=#98C379 gui=nocombine]]
-vim.cmd [[highlight IndentBlanklineIndent5 guifg=#61AFEF gui=nocombine]]
-vim.cmd [[highlight IndentBlanklineIndent6 guifg=#E5C07B gui=nocombine]]
-
--- vim.opt.list = true
-vim.opt.listchars:append "space:⋅"
-vim.opt.listchars:append "eol:↴"
--- blankline end
+vim.opt.termguicolors = true
 
 -- close other buffers and keep current opened only
 -- see: https://tech.serhatteker.com/post/2020-06/close-all-buffers-but-current-in-vim/
--- lua/custom/override.lua
+++ lua/custom/override.lua
@@ -54,16 +54,32 @@ M.nvterm = {
   },
 }
 
-M.blankline = {
-  space_char_blankline = " ",
-  char_highlight_list = {
-    "IndentBlanklineIndent1",
-    "IndentBlanklineIndent2",
-    "IndentBlanklineIndent3",
-    "IndentBlanklineIndent4",
-    "IndentBlanklineIndent5",
-    "IndentBlanklineIndent6",
-  },
-}
+M.blankline = function()
+  vim.cmd [[highlight IndentBlanklineIndent1 guifg=#E06C75 gui=nocombine]]
+  vim.cmd [[highlight IndentBlanklineIndent2 guifg=#C097FC gui=nocombine]]
+  vim.cmd [[highlight IndentBlanklineIndent3 guifg=#56B6C2 gui=nocombine]]
+  vim.cmd [[highlight IndentBlanklineIndent4 guifg=#98C379 gui=nocombine]]
+  vim.cmd [[highlight IndentBlanklineIndent5 guifg=#61AFEF gui=nocombine]]
+  vim.cmd [[highlight IndentBlanklineIndent6 guifg=#E5C07B gui=nocombine]]
+
+  -- vim.opt.list = true
+  -- vim.opt.listchars:append "space:⋅"
+  -- vim.opt.listchars:append "trail:⋅"
+  -- vim.opt.listchars:append "eol:↴"
+
+  return {
+    space_char_blankline = " ",
+    show_current_context = true,
+    show_current_context_start = true,
+    char_highlight_list = {
+      "IndentBlanklineIndent1",
+      "IndentBlanklineIndent2",
+      "IndentBlanklineIndent3",
+      "IndentBlanklineIndent4",
+      "IndentBlanklineIndent5",
+      "IndentBlanklineIndent6",
+    },
+  }
+end
 
 return M
